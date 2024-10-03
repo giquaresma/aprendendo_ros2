c033296fc4d85c3e2a19ed6bdf9a29e965abe7fd
giovanna quaresma <143673977+giquaresma@users.noreply.github.com>
2024-10-03 21:53:15 +0000

    aula 8, inicio

--- src/meu_primeiro_pacote/setup.py
+++ src/meu_primeiro_pacote/setup.py
@@ -26,7 +26,8 @@ setup(
             'listener = meu_primeiro_pacote.listener:main',
             'r2d2 = meu_primeiro_pacote.r2d2:main',
             'r2d2_control = meu_primeiro_pacote.r2d2_control:main',
-            'wavefront = meu_primeiro_pacote.wavefront:main',        
+            'wavefront = meu_primeiro_pacote.wavefront:main',
+            'a_star = meu_primeiro_pacote.a_star:main',        
         ],
     },
 )
